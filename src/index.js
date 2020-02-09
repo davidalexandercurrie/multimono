eab82cf1018a71e5a9c4457369625bcec023ac0a
David Currie <david.alexander.currie@gmail.com>
2020-02-09 17:18:45 +1300

    scroll instructions

--- src/index.js
+++ src/index.js
@@ -4,24 +4,25 @@ import React, { Suspense, useRef, useEffect, useState } from 'react'
 import { Dom, Canvas, extend, useThree, useFrame, useLoader } from 'react-three-fiber'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
 import ClipLoader from 'react-spinners/ClipLoader'
+import { FaPlay } from 'react-icons/fa'
 import './styles.css'
 import { TextGeometry } from 'three'
 
 extend({ OrbitControls })
 
 const tracks = [
-  '/Audio/12xmonobrowser-001.ogg',
-  '/Audio/12xmonobrowser-002.ogg',
-  '/Audio/12xmonobrowser-003.ogg',
-  '/Audio/12xmonobrowser-004.ogg',
-  '/Audio/12xmonobrowser-005.ogg',
-  '/Audio/12xmonobrowser-006.ogg',
-  '/Audio/12xmonobrowser-007.ogg',
-  '/Audio/12xmonobrowser-008.ogg',
-  '/Audio/12xmonobrowser-009.ogg',
-  '/Audio/12xmonobrowser-010.ogg',
-  '/Audio/12xmonobrowser-011.ogg',
-  '/Audio/12xmonobrowser-012.ogg'
+  { url: '/Audio/12xmonobrowser-001.ogg', x: Math.random() * 10 - 5, z: Math.random() * 10 - 5 },
+  { url: '/Audio/12xmonobrowser-002.ogg', x: Math.random() * 10 - 5, z: Math.random() * 10 - 5 },
+  { url: '/Audio/12xmonobrowser-003.ogg', x: Math.random() * 10 - 5, z: Math.random() * 10 - 5 },
+  { url: '/Audio/12xmonobrowser-004.ogg', x: Math.random() * 10 - 5, z: Math.random() * 10 - 5 },
+  { url: '/Audio/12xmonobrowser-005.ogg', x: Math.random() * 10 - 5, z: Math.random() * 10 - 5 },
+  { url: '/Audio/12xmonobrowser-006.ogg', x: Math.random() * 10 - 5, z: Math.random() * 10 - 5 },
+  { url: '/Audio/12xmonobrowser-007.ogg', x: Math.random() * 10 - 5, z: Math.random() * 10 - 5 },
+  { url: '/Audio/12xmonobrowser-008.ogg', x: Math.random() * 10 - 5, z: Math.random() * 10 - 5 },
+  { url: '/Audio/12xmonobrowser-009.ogg', x: Math.random() * 10 - 5, z: Math.random() * 10 - 5 },
+  { url: '/Audio/12xmonobrowser-010.ogg', x: Math.random() * 10 - 5, z: Math.random() * 10 - 5 },
+  { url: '/Audio/12xmonobrowser-011.ogg', x: Math.random() * 10 - 5, z: Math.random() * 10 - 5 },
+  { url: '/Audio/12xmonobrowser-012.ogg', x: Math.random() * 10 - 5, z: Math.random() * 10 - 5 }
 ]
 
 const Controls = props => {
@@ -52,7 +53,7 @@ function Sound({ url }) {
     sound.current.setRefDistance(1)
     sound.current.setLoop(true)
     sound.current.play()
-    sound.current.setDirectionalCone(90, 210, 0.3)
+    sound.current.setDirectionalCone(120, 260, 0.3)
     camera.add(listener)
     return () => camera.remove(listener)
   }, [])
@@ -61,43 +62,67 @@ function Sound({ url }) {
 
 const Loading = () => (
   <Dom>
-    {/* <div class="loading">Loading...</div> */}
-    <ClipLoader className="spinner" size={150} color={'rgb(207, 192, 9)'} />
+    <div className="loading">
+      LOADING...
+      <ClipLoader className="spinner" size={150} color={'black'} />
+    </div>
   </Dom>
 )
 
 function App() {
   const [play, setPlay] = useState(false)
+  const [showControls, setShowControls] = useState(true)
 
   const onClick = () => {
     setPlay(true)
+  }
+  const closeControls = () => {
+    setShowControls(false)
   }
   return (
     <>
       <Canvas camera={{ position: [0, 0, 5] }}>
         {!play && (
-          <Dom>
-            <button className="playButton" onClick={onClick}>
-              //PLAY//
-            </button>
-          </Dom>
+          <>
+            {/* <TextGeometry attach="geometry" args={['asdfasdfasd', {}]} /> */}
+            <Suspense fallback={null}>
+              <Dom position={[100, -50, 100]}>
+                <button className="playButton" onClick={onClick}>
+                  <code>|> 12xMono - David Currie |></code>
+                </button>
+              </Dom>
+            </Suspense>
+          </>
         )}
         {play && (
           <Suspense fallback={<Loading />}>
+            {showControls && (
+              <Dom className="controls" position={[100, 0, 100]}>
+                <p>
+                  Left-Mouse = rotate, Right-Mouse = Pan, Scroll = Zoom{' '}
+                  <button className="closeControls" onClick={closeControls}>
+                    X
+                  </button>
+                </p>
+              </Dom>
+            )}
             <ambientLight></ambientLight>
-            <spotLight castShadow position={[100, 100, 20]}></spotLight>
-
-            {tracks.map(url => (
-              <mesh position={[Math.random() * 10 - 5, 0, Math.random() * 10 - 5]}>
+            <spotLight castShadow intensity={1} angle={Math.PI / 10} position={[10, 10, 10]} shadow-mapSize-width={2048} shadow-mapSize-height={2048} />
+            <spotLight intensity={0.5} castShadow position={[10, 100, 20]}></spotLight>
+            <ambientLight />
+            <pointLight intensity={4} />
+            {tracks.map(node => (
+              <mesh position={[node.x, 0, node.z]}>
                 <boxBufferGeometry attach="geometry" />
-                <meshPhysicalMaterial attach="material" color="rgb(207, 192, 9)" />
-                <Sound url={url} />
+
+                <meshPhysicalMaterial attach="material" color="#222222" />
+                <Sound url={node.url} />
               </mesh>
             ))}
           </Suspense>
         )}
 
-        <Controls enableKeys={true} zoomSpeed={0.25} />
+        <Controls autoRotateSpeed={0.5} autoRotate={play ? false : true} zoomSpeed={0.25} />
       </Canvas>
     </>
   )
