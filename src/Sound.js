551af7f6140253e42bc16b2e080dec8f66c277a7
David Currie <david.alexander.currie@gmail.com>
2020-06-23 00:57:46 +1200

    add hover animation and raycaster

--- src/Sound.js
+++ src/Sound.js
@@ -1,15 +1,28 @@
-import React, { useRef, useState, useEffect } from 'react'
+import React, { useRef, useState, useEffect, useCallback } from 'react'
 import * as THREE from 'three'
 import { useThree, useLoader } from 'react-three-fiber'
-import { AudioAnalyser } from 'three'
+import { useSpring, a } from 'react-spring/three'
 
 export default function Sound({ url, node, playAudio }) {
   const sound = useRef()
   const analyzer = useRef()
   const ref = useRef()
+  const [hovered, setHovered] = useState(false)
   const { camera } = useThree()
   const [listener] = useState(() => new THREE.AudioListener())
   const buffer = useLoader(THREE.AudioLoader, url)
+  const props = useSpring({
+    color: hovered ? '#123254' : '#324353'
+  })
+
+  const onHover = useCallback(
+    (e, value) => {
+      e.stopPropagation() // stop it at the first intersection
+      setHovered(value)
+    },
+    [setHovered]
+  )
+  //...
 
   if (playAudio) {
     sound.current.play()
@@ -25,6 +38,7 @@ export default function Sound({ url, node, playAudio }) {
     sound.current.setVolume(1)
     // sound.current.setDirectionalCone(120, 260, 0.3)
     camera.add(listener)
+    console.log(camera.position.x)
     return () => {
       camera.remove(listener)
       sound.current.pause()
@@ -32,11 +46,17 @@ export default function Sound({ url, node, playAudio }) {
   }, [])
 
   return (
-    <mesh castShadow rotation={[0, node.rotation, 0]} key={node.id} position={[node.x / 1.5, 0, node.z / 1.5]}>
+    <a.mesh
+      onPointerOver={(e) => onHover(e, true)}
+      onPointerOut={(e) => onHover(e, false)}
+      castShadow
+      rotation={[0, node.rotation, 0]}
+      key={node.id}
+      position={[node.x / 1.5, 0, node.z / 1.5]}>
       <boxBufferGeometry attach="geometry" />
-      <meshPhysicalMaterial ref={ref} attach="material" color="#324353" />
+      <a.meshPhysicalMaterial ref={ref} attach="material" color={props.color} />
       <positionalAudio ref={sound} args={[listener]} />
       {/* {buffer && <AudioAnalyser ref={analyzer} args={[buffer, 32]} />} */}
-    </mesh>
+    </a.mesh>
   )
 }
